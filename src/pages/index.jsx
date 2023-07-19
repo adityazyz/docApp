ad97207e92f7d8fbc36292760797eda10f20956c
adityazyzz <adityazyzz@gmail.com>
2023-07-20 00:16:58 +0530

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import { useState } from "react";
+
+export default function Document() {
+
+  
+  return (
+    <div>
+      
+      homepage after update
+
+    </div>
+  );
+}
