dee234bc3877af663d30e0f8f136fdc22c926e43
adityazyzz <adityazyzz@gmail.com>
2023-07-22 18:04:08 +0530

    admin navbar in process

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,9 +4,15 @@ import Script from "next/script";
 import "../styles/globals.css"
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
+import { useRouter } from "next/router";
+import AdminNavbar from "../../components/AdminNavbar";
+
 // import "../styles/admin-globals.css"     << will import directly in admin pages
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter();
+
   return (
     <>
       <Head>
@@ -18,9 +24,16 @@ function MyApp({ Component, pageProps }) {
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
         crossOrigin="anonymous"
       />
-      <Navbar/>
+
+      
+      {/* // show admin navbar if admin page open  */}
+      {(router.pathname === "/admin" ) && <AdminNavbar/>}
+      
+      {/* // don't show normal navbar if admin page open  */}
+      {(router.pathname !== "/admin" ) && <Navbar/>}
       <Component {...pageProps} />
-      <Footer/>
+      {/* // don't show normal footer if admin page open  */}
+      {(router.pathname !== "/admin" ) && <Footer/>}
 
     </>
   );
